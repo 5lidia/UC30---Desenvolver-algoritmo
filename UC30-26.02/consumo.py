fb93dcaa9c093e8973984a77d47c15784b2def1e
5lidia <lidiaothilia5@gmail.com>
2026-02-26 18:27:43 -0300

    Add files via upload

---
+++ UC30-26.02/consumo.py
@@ -0,0 +1,9 @@
+distancia = float(input("Digite a distância pecorrida:"))
+combustivel = float(input("Digite a quantidade de combustível gasta (litros): "))
+
+print(type(distancia))
+print(type(combustivel))
+
+cosumo = distancia / combustivel 
+
+print(consumo)
