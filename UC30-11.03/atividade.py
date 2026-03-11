fb76bfe4dad22b6a297eb2891e3b831f805d9aa7
5lidia <lidiaothilia5@gmail.com>
2026-03-11 15:10:57 -0300

    atividade.py

---
+++ UC30-11.03/atividade.py
@@ -0,0 +1,30 @@
+
+Lídia Othilia <lidiaothilia5@gmail.com>
+8:18 AM (6 hours ago)
+to me
+
+aluno = {}
+
+
+aluno["nome"] = input("Digite o nome do aluno: ")
+aluno["nota1"] = float(input("Digite a nota da prova 1: "))
+aluno["nota2"] = float(input("Digite a nota da prova 2: "))
+
+media = (aluno["nota1"] + aluno["nota2"]) / 2
+
+
+aluno["media"] = media
+
+
+print("\nDados do aluno:")
+for chave, valor in aluno.items():
+    print(f"{chave}: {valor}")
+
+if media >= 7:
+    situacao = "Aprovado"
+elif media >= 5:
+    situacao = "Recuperação"
+else:
+    situacao = "Reprovado"
+
+print("Situação do aluno:", situacao)
