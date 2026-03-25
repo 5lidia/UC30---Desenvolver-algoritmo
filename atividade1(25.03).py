ebac1284480666fb7b575d9e8bf3e41eaf6dea3f
5lidia <lidiaothilia5@gmail.com>
2026-03-25 15:55:32 -0300

    Add files via upload

---
+++ atividade1(25.03).py
@@ -0,0 +1,14 @@
+def calcular_estatisticas(*notas) :
+    if not notas:
+        return 0, 0, 0, 0
+    
+    soma = sum(notas)
+    media = soma / len(notas)
+    maior = max(notas)
+    menor = min(notas)
+
+    return soma, media, maior, menor
+
+soma, media, max_nota, min_nota = calcular_estatisticas(7, 8, 9)
+print(f"Média: {media} Soma: {soma} Maior: {max_nota} Menor: {min_nota}")
+        
